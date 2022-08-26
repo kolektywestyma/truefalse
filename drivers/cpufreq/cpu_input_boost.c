cyberknight777/dragonheart_kernel_oneplus_sm8150

cpu_input_boost: Allow configuration of LP and HP cluster's minimum frequency
Inspired by: https://github.com/ConquerOS-Devices/kernel_xiaomi_sdm439/commit/6d2ce8be89b3c681f0ad7feb2bc844705264fab6

Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

drivers/cpufreq/cpu_input_boost.c
@@ -72,6 +72,18 @@ static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
 	return min(freq, policy->max);
 }
 
+static unsigned int get_min_freq(struct cpufreq_policy *policy)
+{
+  struct boost_drv *b = &boost_drv_g;
+  unsigned int freq;
+
+  if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+    freq = CONFIG_REMOVE_INPUT_BOOST_FREQ_LP;
+  else
+    freq = CONFIG_REMOVE_INPUT_BOOST_FREQ_PERF;
+  return max(freq, policy->cpuinfo.min_freq);
+}
+
 static void update_online_cpu_policy(void)
 {
 	unsigned int cpu;
@@ -194,7 +206,7 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 
 	/* Unboost when the screen is off */
 	if (test_bit(SCREEN_OFF, &b->state)) {
-		policy->min = policy->cpuinfo.min_freq;
+	        policy->min = get_min_freq(policy);
 		return NOTIFY_OK;
 	}
 
@@ -223,7 +235,7 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 	if (test_bit(INPUT_BOOST, &b->state))
 		policy->min = get_input_boost_freq(policy);
 	else
-		policy->min = policy->cpuinfo.min_freq;
+	        policy->min = get_min_freq(policy);
 
 	return NOTIFY_OK;
 }

kolektywestyma/truefalse

drivers/cpufreq/cpu_input_boost.c
@@ -73,6 +73,18 @@ static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
 	return min(freq, policy->max);
 }
 
+static unsigned int get_min_freq(struct cpufreq_policy *policy)
+{
+  struct boost_drv *b = &boost_drv_g;
+  unsigned int freq;
+
+  if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+    freq = CONFIG_MIN_FREQ_LP;
+  else
+    freq = CONFIG_MIN_FREQ_PERF;
+  return max(freq, policy->cpuinfo.min_freq);
+}
+
 static void update_online_cpu_policy(void)
 {
 	unsigned int cpu;
@@ -195,7 +207,7 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 
 	/* Unboost when the screen is off */
 	if (test_bit(SCREEN_OFF, &b->state)) {
-		policy->min = policy->cpuinfo.min_freq;
+	        policy->min = get_min_freq(policy);
 		return NOTIFY_OK;
 	}
 
@@ -224,7 +236,7 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 	if (test_bit(INPUT_BOOST, &b->state))
 		policy->min = get_input_boost_freq(policy);
 	else
-		policy->min = policy->cpuinfo.min_freq;
+	        policy->min = get_min_freq(policy);
 
 	return NOTIFY_OK;
 }